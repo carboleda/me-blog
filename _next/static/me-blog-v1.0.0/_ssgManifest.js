e363748b62f5fa497e14f25d94cff7183fb86656
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-18 00:56:15 +0000

    Deploy carboleda/me-blog to github.com/carboleda/me-blog.git:master

--- _next/static/me-blog-v1.0.0/_ssgManifest.js
+++ _next/static/me-blog-v1.0.0/_ssgManifest.js
@@ -1,1 +1,1 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Fblog"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
+self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002F","\u002Fblog"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
